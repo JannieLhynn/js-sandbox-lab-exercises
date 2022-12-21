c285bd8533b237ddaaef6cda63419e838e68bce3
Jannie Lyn <116628447+JannieLhynn@users.noreply.github.com>
2022-12-22 01:09:48 +0800

    Update challenge.js

--- part-1-basics/1-functions/challenge.js
+++ part-1-basics/1-functions/challenge.js
@@ -3,9 +3,21 @@
 // Create a function that will calculate the year of retirement of the user
 // Create a function that will calculate the year remaining for the user to retire
 
-const c = console.log.bind(this);
-const yearNow = new Date().getFullYear();
+const userName = 'Jane';
+function calcUserAge(userBirthYear) { // Calculate User Age
+	const yearNow = new Date().getFullYear();
+	const userAge = yearNow - userBirthYear;
+	const retirementYear = yearNow + (50 - userAge);
+	const retiredYear = retirementYear - yearNow; // Calculate Retirement Age
+	if (userAge > 50) {
+		console.log(`${userName} is ready for retirement`);
+	} else {
+		console.log(`${userName} is ${userAge} years old and can retire at the year ${retirementYear} with ${retiredYear} years remaining at work`)
+	}
+}
 
-// Calculate User Age
+calcUserAge(2000);
 
-// Calculate Retirement Age
+
+
+
